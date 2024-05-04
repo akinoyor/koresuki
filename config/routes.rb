b7ff5ba9325eb81c9e99abcd98227660745a4c27
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-04 02:38:19 +0000

    [add]コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  namespace :public do
+    get 'bookmarks/index'
+  end
+  namespace :public do
+    get 'comments/new'
+    get 'comments/index'
+    get 'comments/show'
+    get 'comments/edit'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+  end
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
