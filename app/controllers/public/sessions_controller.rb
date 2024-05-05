5082998201e9b9bb4448901de05320b544f54af2
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-05 05:22:03 +0000

    [add]ゲストログイン機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,12 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+    sign_in user
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
 
   scope module: :public do
     devise_for :users
+    devise_scope :user do
+    post  '/users/guest_sign_in', to: 'sessions#guest_sign_in', as: 'guest_sign_in'
+    end
     root to: 'homes#top'
     resource  :user,  only:[:edit, :update, :destroy]
-    get '/user/bookmarks',  to: 'bookmarks#index',as: 'bookmarks'
-    get   '/user/may_page', to: 'users#show',     as: 'my_page'
-    post  '/user/confirm',  to: 'users#confirm',  as: 'confirm'
-    
+    get   '/user/bookmarks',      to: 'bookmarks#index',        as: 'bookmarks'
+    get   '/user/may_page',       to: 'users#show',             as: 'my_page'
+    post  '/user/confirm',        to: 'users#confirm',          as: 'confirm'
+
+
 
     resources :posts, only:[:new, :index, :show, :create, :edit, :update, :destroy] do
       resources  :comments,   only: [:new, :create, :index, :show, :edit, :update, :destroy]
