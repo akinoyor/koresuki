e7b7a8133e3b62bfa3c743f9967b030361447094
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-05 21:04:22 +0000

    [add]投稿 詳細画面編集画面

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -24,16 +24,37 @@ class Public::PostsController < ApplicationController
   end
 
   def show
+     @newpost = Post.new
+     @user = current_user
+     @post = Post.find(params[:id])
+
   end
 
   def edit
+    @newpost = Post.new
+    @user = current_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to posts_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "投稿の更新が完了しました。"
+      redirect_to post_path(@post.id)
+    else
+      @post = @post
+      render :edit
+    end
   end
 
   def destroy
     post = Post.find(params[:id])
     if post.user_id == current_user.id
       post.destroy
-      redirect_to  request.referer
+      redirect_to  posts_path
     end
   end
 
