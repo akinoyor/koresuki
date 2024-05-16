aba316cb2bcb4ef1ad6b1cfa16f354a93f208b62
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-16 15:03:27 +0000

    [fix]routesのインデント整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # 管理者の新規登録とパスワードスキップ↓
+  # 管理者の新規登録パスワードスキップ↓
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
@@ -24,11 +24,11 @@ Rails.application.routes.draw do
       resource  :follows, only: [:create, :destroy]
       resources  :presets,  only: [:create, :destroy, :edit, :update]
     end
-    get     '/partial_ajax(/:id)',         to: 'posts#partial_ajax'
+    get     '/partial_ajax(/:id)',    to: 'posts#partial_ajax'
     get     '/user/bookmarks',        to: 'bookmarks#index',    as: 'bookmarks'
     get     'posts/search',           to: 'posts#search',       as: 'search'
-    get     'posts/serch_words',       to: 'posts#search_words', as: 'search_words'
-    get     'posts/search_names',      to: 'posts#search_names',  as: 'search_names'
+    get     'posts/serch_words',      to: 'posts#search_words', as: 'search_words'
+    get     'posts/search_names',     to: 'posts#search_names', as: 'search_names'
     resources :posts, only:[:index, :show, :create, :edit, :update, :destroy] do
       resources :comments,  only: [:create, :show, :destroy]
       resource  :bookmark,  only: [:create, :destroy]
