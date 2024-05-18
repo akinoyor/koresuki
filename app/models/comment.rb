222a1b21bf8e7805854fc201bf46a201696144d8
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-18 09:33:50 +0000

    [fix]投稿日時更新日時の表記変更テンプレート化

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,9 @@ class Comment < ApplicationRecord
   validates :body, length: { maximum: 50 }
   validates :image, presence: true, if: -> { body.blank? }
   validates :body, presence: true, if: -> { image.blank? }
+
+  def created_at_jp
+    created_at.in_time_zone('Asia/Tokyo').strftime('%Y年%m月%d日 %H時%M分')
+  end
+
 end
