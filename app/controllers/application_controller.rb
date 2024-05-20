b387da0d5ad612f5a8232e3686903145e877fe83
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-05-20 08:49:19 +0000

    [fix]コメントアウト忘れ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_controller?
-  # unless Rails.env.development?
+  unless Rails.env.development?
     rescue_from Exception,                      with: :_render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
     rescue_from ActionController::RoutingError, with: :_render_404
-  # end
+  end
 
   def routing_error
     raise ActionController::RoutingError, params[:path]
