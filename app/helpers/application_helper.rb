2dcb63410daa77da8928fc9dd7992bf634a0aba9
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-11 10:52:51 +0000

    [add]投稿のレスポンシブ対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def formatted_date_jp(date)
     date.in_time_zone('Asia/Tokyo').strftime('%Y年%m月%d日 %H時%M分')
   end
+  def simplified_date_jp(date)
+    date.in_time_zone('Asia/Tokyo').strftime('%Y/%m/%d %H:%M')
+  end
 end
